b4d0012e1c79060e34ee1f360c4d001bb66c8901
David Kreitschmann <neffs1@gmail.com>
2021-05-30 21:53:01 +0200

    Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='cometblue_lite',
-    version='0.3.2',
+    version='0.4.0',
     packages=['cometblue_lite'],
     python_requires='>=3.4',
     install_requires=['bluepy>=1.3'],
